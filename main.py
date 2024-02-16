f9ee7481a18d76c7e7a006ffa7202a318980b55e
saivenkatesh <saivenkatesh619@gmail.com>
2024-02-16 20:09:58 +0530

    Implement browse image feature

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import PIL.Image
 import streamlit as st
 import PIL as pillow
+
+
 with st.expander("Open Camera"):
     #start camera
     captured_image = st.camera_input("camera")
+
+with st.expander("Open Image from Local"):
+    captured_image = st.file_uploader("upload")
 
 if captured_image:
 #creating a instance
